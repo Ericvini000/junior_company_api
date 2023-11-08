54739418c4f132e905c9c5f81a274c9c73dbacc6
ericvini000 <ericviniciusfelixsilva@gmail.com>
2023-11-08 16:55:52 -0400

    feat: Account model created

--- _core/settings.py
+++ _core/settings.py
@@ -30,7 +30,9 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+MY_APPS = ["accounts"]
+
+DJANGO_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -38,6 +40,10 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 ]
+
+THIRD_PARTY_APPS = ["rest_framework"]
+
+INSTALLED_APPS = MY_APPS + DJANGO_APPS + THIRD_PARTY_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -121,3 +127,4 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+AUTH_USER_MODEL = "accounts.Account"
---
+++ accounts/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
+
+
+class ExperienceLevel(models.TextChoices):
+    JUNIOR = "junior"
+    JUNIOR1 = "JUNIOR"
+    JUNIOR2 = "Junior junior"
+
+
+class WorkStatus(models.TextChoices):
+    EMPLOYED = "employed"
+    UNEMPLOYED = "unemployed"
+
+
+class Account(AbstractUser):
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4())
+    profile_url = models.URLField(max_length=200)
+    experience = models.CharField(
+        max_length=14, choices=ExperienceLevel.choices, default=ExperienceLevel.JUNIOR
+    )
+    status = models.CharField(
+        max_length=14, choices=WorkStatus.choices, default=WorkStatus.UNEMPLOYED
+    )
